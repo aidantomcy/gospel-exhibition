45c16c82d67a608a72a64e6c932056faa2d05937
Aidan Tomcy <aidantomcy@gmail.com>
2022-12-02 20:10:57 +0530

    add separate files for routes for individual gospels

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,9 @@
 import express from "express";
 import { config } from "dotenv";
+import gospelMatthewRouter from "./routes/gospelMatthew";
+import gospelMarkRouter from "./routes/gospelMark";
+import gospelLukeRouter from "./routes/gospelLuke";
+import gospelJohnRouter from "./routes/gospelJohn";
 
 config();
 const app = express();
@@ -7,8 +11,12 @@ const PORT = process.env.PORT ?? 5000;
 
 app.get("/", (req, res) => {
   res.json({
-    message: "hey",
+    gospels: ["matthew", "mark", "luke", "john"],
   });
 });
+app.use("/gospels/matthew", gospelMatthewRouter);
+app.use("/gospels/mark", gospelMarkRouter);
+app.use("/gospels/luke", gospelLukeRouter);
+app.use("/gospels/john", gospelJohnRouter);
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
