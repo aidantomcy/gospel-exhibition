cf4a074039711eec7224faceea928ca207114eb4
Aidan Tomcy <aidantomcy@gmail.com>
2022-12-03 07:11:22 +0530

    use controllers for get requests

--- server/src/routes/gospelMatthew.ts
+++ server/src/routes/gospelMatthew.ts
@@ -1,9 +1,8 @@
 import { Router } from "express";
+import { getParables } from "../controllers/gospelMatthewController";
 
 const router = Router();
 
-router.route("/").get((req, res) => {
-  res.status(200).json({ parables: {} });
-});
+router.route("/").get(getParables);
 
 export default router;
