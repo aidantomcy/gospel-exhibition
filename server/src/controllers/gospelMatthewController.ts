48d56b34151f36f34beba5732050942c6224b942
Aidan Tomcy <aidantomcy@gmail.com>
2022-12-04 15:54:23 +0530

    add basic crud functionality in all controller files

--- server/src/controllers/gospelMatthewController.ts
+++ server/src/controllers/gospelMatthewController.ts
@@ -7,4 +7,58 @@ const getParables = expressAsyncHandler(async (req, res) => {
   res.status(200).json(parables);
 });
 
-export { getParables };
+const addParable = expressAsyncHandler(async (req, res) => {
+  type Data = {
+    title: string;
+    body: string;
+    explanation: string;
+  };
+
+  const { title, body, explanation }: Data = req.body ?? {
+    title: "",
+    body: "",
+    explanation: "",
+  };
+
+  if (title === "" || body === "" || explanation === "") {
+    res.status(400);
+    throw new Error("please provide data for all fields");
+  }
+
+  const parable = await Parable.create({
+    gospel: "matthew",
+    body,
+    explanation,
+    title,
+  });
+
+  res.status(201).json(parable);
+});
+
+const updateParable = expressAsyncHandler(async (req, res) => {
+  const parable = await Parable.findById(req.params.id);
+
+  if (!parable) {
+    res.status(400);
+    throw new Error("parable not found");
+  }
+
+  const updatedParable = Parable.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  res.status(400).json(updatedParable);
+});
+
+const deleteParable = expressAsyncHandler(async (req, res) => {
+  const parable = await Parable.findById(req.params.id);
+
+  if (!parable) {
+    res.status(400);
+    throw new Error("parable not found");
+  }
+
+  await parable.remove();
+  res.status(200).json({ id: req.params.id });
+});
+
+export { getParables, addParable, updateParable, deleteParable };
