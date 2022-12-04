cfb52e20283ee045b9964fec5e39b1eca056de32
Aidan Tomcy <aidantomcy@gmail.com>
2022-12-04 20:29:00 +0530

    change export style in constants.ts

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -2,7 +2,6 @@ import { config } from "dotenv";
 
 config();
 
-const PORT = process.env.PORT ?? 5000;
-const DB_URL = process.env.DB_URL ?? "";
-
-export { PORT, DB_URL };
+export const PORT = process.env.PORT ?? 5000;
+export const DB_URL = process.env.DB_URL ?? "";
+export const NODE_ENV = process.env.NODE_ENV;
