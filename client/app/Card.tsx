77b3fa42456debea35fdba208a657fd901ac3320
Aidan Tomcy <aidantomcy@gmail.com>
2022-12-16 21:19:30 +0530

    add card component

---
+++ client/app/Card.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+
+type CardProps = {
+  title: string;
+  image: string;
+  description: string;
+};
+
+const Card = ({ title, image, description }: CardProps) => {
+  return (
+    <div className="my-6 bg-gray-200 p-4 rounded-xl grid place-items-center select-none h-72 w-52 mx-40 hover:-translate-y-1 hover:cursor-pointer hover:shadow-lg hover:duration-150">
+      <Image
+        src={image}
+        alt="image cannot be loaded"
+        width="150"
+        height="150"
+        className="rounded-2xl"
+      />
+      <div className="text-center py-2">{title}</div>
+    </div>
+  );
+};
+
+export default Card;
