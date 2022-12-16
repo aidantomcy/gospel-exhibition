a49ff765b9242a64fe0277fe43a1b2223391ea7c
Aidan Tomcy <aidantomcy@gmail.com>
2022-12-16 21:20:31 +0530

    display list of all four evangelists with the `Card` component

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,7 +1,18 @@
+import Card from "./Card";
+
 const HomePage = () => {
   return (
-    <div>
-      <h1 className="text-3xl">hello, world</h1>
+    <div className="grid place-items-center">
+      <div className="grid max-w-3xl place-items-center grid-cols-2">
+        <Card
+          title="Matthew the Evangelist"
+          image="/stmatthew.jpg"
+          description=""
+        />
+        <Card title="Mark the Evangelist" image="/stmark.jpg" description="" />
+        <Card title="Luke the Evangelist" image="/stluke.jpg" description="" />
+        <Card title="John the Evangelist" image="/stjohn.jpg" description="" />
+      </div>
     </div>
   );
 };
