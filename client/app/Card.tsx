beb8d96d30a5bc09199eccafd26f63ddbe61b068
Aidan Tomcy <aidantomcy@gmail.com>
2022-12-23 19:07:32 +0530

    remove extra div in card component

--- client/app/Card.tsx
+++ client/app/Card.tsx
@@ -10,17 +10,18 @@ type CardProps = {
 
 const Card = ({ title, image, url, description }: CardProps) => {
   return (
-    <Link href={url}>
-      <div className="my-6 bg-gray-200 p-4 rounded-xl grid place-items-center select-none h-72 w-52 mx-40 hover:-translate-y-1 hover:cursor-pointer hover:shadow-lg hover:duration-150">
-        <Image
-          src={image}
-          alt="image cannot be loaded"
-          width={150}
-          height={150}
-          className="rounded-2xl"
-        />
-        <div className="text-center py-2">{title}</div>
-      </div>
+    <Link
+      href={url}
+      className="my-6 bg-gray-200 p-4 rounded-xl grid place-items-center select-none h-72 w-52 mx-40 hover:-translate-y-1 hover:cursor-pointer hover:shadow-lg hover:duration-150"
+    >
+      <Image
+        src={image}
+        alt="image cannot be loaded"
+        width={150}
+        height={150}
+        className="rounded-2xl"
+      />
+      <div className="text-center py-2">{title}</div>
     </Link>
   );
 };
