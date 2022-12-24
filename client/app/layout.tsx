3036ea0903ec2d374fbd2a72713375b2cb9f720c
Aidan Tomcy <aidantomcy@gmail.com>
2022-12-24 11:39:07 +0530

    set overflow-x to hidden for main html tag

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -8,7 +8,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html>
+    <html className="overflow-x-hidden">
       <head></head>
       <body>
         <Navbar />
