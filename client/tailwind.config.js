5a5f6e4157375262b02c79f5dcf35c26b2022288
Aidan Tomcy <aidantomcy@gmail.com>
2022-12-24 13:40:34 +0530

    update taiwind config and add class name to use inter font

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./app/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ["var(--font-inter)"],
+      },
+    },
   },
   plugins: [],
 };
