df98966e0b4750bfbbdce0db6d75d18fa44a02c6
Aidan Tomcy <aidantomcy@gmail.com>
2022-12-24 08:14:16 +0530

    add back button

--- client/app/[gospel]/page.tsx
+++ client/app/[gospel]/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Card from "../Card";
 
 const getParables = async (gospel: Gospels): Promise<ApiResponse[]> => {
@@ -13,22 +14,44 @@ const Parables = async ({ params }: { params: { gospel: Gospels } }) => {
   const data = await getParables(params.gospel);
 
   return (
-    <div className="grid place-items-center w-screen">
-      <div className="grid place-items-center grid-cols-4 gap-8 max-w-5xl">
-        {data.map((parable) => {
-          const { _id: id, title } = parable;
-          return (
-            <Card
-              key={id}
-              title={title}
-              image={`/${id}-thumbnail.jpg`}
-              url={`/${params.gospel}/${id}`}
-              description=""
-            />
-          );
-        })}
+    <>
+      <Link
+        href="/"
+        className="text-xl flex items-center justify-center w-20 bg-slate-100 ml-36 mt-6 p-2 rounded-md hover:bg-slate-200 hover:shadow-sm"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-4 h-4"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15.75 19.5L8.25 12l7.5-7.5"
+          />
+        </svg>
+        Back
+      </Link>
+      <div className="grid place-items-center w-screen">
+        <div className="grid place-items-center grid-cols-4 gap-8 max-w-5xl">
+          {data.map((parable) => {
+            const { _id: id, title } = parable;
+            return (
+              <Card
+                key={id}
+                title={title}
+                image={`/${id}-thumbnail.jpg`}
+                url={`/${params.gospel}/${id}`}
+                description=""
+              />
+            );
+          })}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
