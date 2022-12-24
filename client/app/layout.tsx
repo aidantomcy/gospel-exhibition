87f110a2e85e12cb390a512587d5b8a9f945cab4
Aidan Tomcy <aidantomcy@gmail.com>
2022-12-24 13:40:47 +0530

    update layout.tsx to use inter font

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,6 +1,12 @@
 /* eslint-disable @next/next/no-head-element */
 import "./globals.css";
 import Navbar from "./Navbar";
+import { Inter } from "@next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
 
 export default function RootLayout({
   children,
@@ -8,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html className="overflow-x-hidden">
+    <html className={`${inter.variable} font-inter overflow-x-hidden`}>
       <head></head>
       <body>
         <Navbar />
