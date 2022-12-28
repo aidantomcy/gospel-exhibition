9a30e966692bbd22e409f8373e3d28e4e0fc5ce8
Aidan Tomcy <aidantomcy@gmail.com>
2022-12-28 17:14:40 +0530

    increase card componenet height and add `object-fit: cover;` for card images

--- client/app/Card.tsx
+++ client/app/Card.tsx
@@ -12,14 +12,14 @@ const Card = ({ title, image, url, description }: CardProps) => {
   return (
     <Link
       href={url}
-      className="my-6 bg-gray-200 p-4 rounded-xl grid place-items-center select-none h-72 w-52 mx-40 hover:-translate-y-1 hover:cursor-pointer hover:shadow-lg hover:duration-150"
+      className="my-6 bg-gray-200 p-4 rounded-xl grid place-items-center select-none h-80 w-52 mx-40 hover:-translate-y-1 hover:cursor-pointer hover:shadow-lg hover:duration-150"
     >
       <Image
         src={image}
         alt="image cannot be loaded"
         width={150}
         height={150}
-        className="rounded-2xl"
+        className="rounded-2xl w-40 h-52 object-cover"
       />
       <div className="text-center py-2">{title}</div>
     </Link>
