486bdb0a6ae82fc637f2f7d16209b433245bf0d6
Aidan Tomcy <aidantomcy@gmail.com>
2023-01-17 07:11:54 +0530

    clean up index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,8 @@
 import express from "express";
-import gospelMatthewRouter from "./routes/gospelMatthew";
-import gospelMarkRouter from "./routes/gospelMark";
-import gospelLukeRouter from "./routes/gospelLuke";
-import gospelJohnRouter from "./routes/gospelJohn";
 import { PORT } from "./constants";
 import connectDB from "./db/connectDB";
 import errorHandler from "./middleware/errorMiddleware";
+import registerRouter from "./routes/router";
 
 connectDB();
 const app = express();
@@ -13,10 +10,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(errorHandler);
-app.use("/gospels/matthew", gospelMatthewRouter);
-app.use("/gospels/mark", gospelMarkRouter);
-app.use("/gospels/luke", gospelLukeRouter);
-app.use("/gospels/john", gospelJohnRouter);
+app.use("/gospels/matthew", registerRouter("matthew"));
+app.use("/gospels/mark", registerRouter("mark"));
+app.use("/gospels/luke", registerRouter("luke"));
+app.use("/gospels/john", registerRouter("john"));
 
 app.get("/", (req, res) => {
   res.status(200).json({
