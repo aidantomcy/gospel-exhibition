1900c7d8d38866956e7e469244de0ecf0e30ec90
Aidan Tomcy <aidantomcy@gmail.com>
2023-01-18 07:43:13 +0530

    add bg-dark class for colors in tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./app/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
     extend: {
       fontFamily: {
         inter: ["var(--font-inter)"],
+      },
+      colors: {
+        dark: "#1f1f1f",
       },
     },
   },
