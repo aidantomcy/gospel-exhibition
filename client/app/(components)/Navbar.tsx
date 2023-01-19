5d75672395ddabb29cedb9f1a58dc0944b77061e
Aidan Tomcy <aidantomcy@gmail.com>
2023-01-19 07:11:21 +0530

    change navbar appearance in dark mode

--- client/app/(components)/Navbar.tsx
+++ client/app/(components)/Navbar.tsx
@@ -3,7 +3,7 @@ import ThemeButton from "./ThemeButton";
 
 const Navbar = () => {
   return (
-    <nav className="flex justify-around py-4 bg-slate-100 h-16 items-center">
+    <nav className="flex justify-around py-4 bg-slate-100 h-16 items-center dark:bg-secondary dark:text-white">
       <Link
         className="text-2xl hover:underline hover:decoration-sky-500 hover:decoration-wavy"
         href="/"
