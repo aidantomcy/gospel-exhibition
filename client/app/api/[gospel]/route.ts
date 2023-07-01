13fa65cbb192352a2bf5e012dfef5a4e0d6903d5
Aidan Tomcy <aidantomcy@gmail.com>
2023-07-01 19:25:40 +0530

    add api route to fetch all parables and to add new parables

---
+++ client/app/api/[gospel]/route.ts
@@ -0,0 +1,52 @@
+import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+import { z } from "zod";
+
+export const GET = async (
+  request: Request,
+  { params }: { params: { gospel: string } }
+) => {
+  const prisma = new PrismaClient();
+  const parables = await prisma.parable.findMany({
+    where: { gospel: params.gospel },
+  });
+
+  return NextResponse.json(parables);
+};
+
+export const POST = async (
+  request: Request,
+  { params }: { params: { gospel: string } }
+) => {
+  const prisma = new PrismaClient();
+  const ParableSchema = z.object({
+    title: z.string(),
+    body: z.string(),
+    explanation: z.string(),
+    verses: z.string(),
+  });
+
+  type Parable = z.infer<typeof ParableSchema>;
+
+  const res: Parable = await request.json();
+  const result = ParableSchema.safeParse(res);
+
+  if (!result) {
+    throw new Error("please provide all fields required.");
+  } else if (!["matthew", "mark", "luke", "john"].includes(params.gospel)) {
+    throw new Error("invalid gospel name provided");
+  }
+
+  const { title, body, explanation, verses } = res;
+  const newParable = await prisma.parable.create({
+    data: {
+      title,
+      body,
+      explanation,
+      verses,
+      gospel: params.gospel,
+    },
+  });
+
+  return NextResponse.json(newParable);
+};
