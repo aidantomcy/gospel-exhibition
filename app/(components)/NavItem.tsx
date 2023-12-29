5d0170822988225e53bb277857651af62c7dbb86
Aidan Tomcy <aidantomcy@gmail.com>
2023-12-29 11:26:11 +0530

    Update NavItem and Navbar styling

--- app/(components)/NavItem.tsx
+++ app/(components)/NavItem.tsx
@@ -1,11 +1,11 @@
 import Link from "next/link";
 
-type Props = { text: string , href: string }
+type Props = { text: string; href: string };
 
 const NavItem = ({ text, href }: Props) => {
   return (
     <Link
-      className="text-2xl hover:underline hover:decoration-sky-500 hover:decoration-wavy"
+      className="font-normal hover:underline hover:decoration-sky-500 hover:decoration-wavy px-2 md:text-xl md:px-4"
       href={href}
     >
       {text}
--- app/(components)/Navbar.tsx
+++ app/(components)/Navbar.tsx
@@ -2,11 +2,13 @@ import NavItem from "./NavItem";
 
 const Navbar = () => {
   return (
-    <nav className="flex justify-around py-4 bg-slate-100 h-16 items-center dark:bg-dark-secondary dark:text-white">
-      <NavItem text="Home" href="/" />
-      <NavItem text="Contact" href="/contact" />
-      <NavItem text="About the Creator" href="https://aidantomcy.github.io" />
-    </nav>
+    <div className="w-full flex justify-center mt-4">
+      <nav className="bg-gray-200 rounded-2xl flex justify-center items-center dark:bg-dark-secondary w-11/12 md:w-3/4 lg:w-1/2 dark:text-white h-14">
+        <NavItem text="Home" href="/" />
+        <NavItem text="Contact" href="/contact" />
+        <NavItem text="About the Creator" href="https://aidantomcy.github.io" />
+      </nav>
+    </div>
   );
 };
 
